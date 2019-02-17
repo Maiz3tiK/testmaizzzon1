b60fa36adf24b80dc5171b56aaa403915b984b66
Maizzzon <46628344+Maiz3tiK@users.noreply.github.com>
2019-02-17 01:20:44 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -8,7 +8,7 @@ bot.on('ready', function () {
     console.log("Connectedç");
 });
 
-bot.login("NTQ2NDAxODg4Nzc4ODQ2MjA5.D0n5oQ.CWo-P4IvzvJo0rs_zXURM0BH1Pc");
+bot.login(process.env.TOKEN);
 
 
 bot.on('message',message => {
